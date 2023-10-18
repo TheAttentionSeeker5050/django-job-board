bf767b5a6905f7772673a3094e9ad5e93073810f
nicolas <nicolas.castellano.6@hotmail.com>
2023-10-17 22:32:43 -0300

    added skills visual widget to the resume

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MyApplicantProfilesListView
+from .views import MyApplicantProfilesListView, JobApplicantCreateView
 
 # add the routes for the views here
 
 urlpatterns = [
     path('', MyApplicantProfilesListView.as_view(), name='my_resumes_list'),
+    path('create/', JobApplicantCreateView.as_view(), name='my_resumes_create'),
 ]
--- resumes/views.py
+++ resumes/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Resume, JobApplicant
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views import View
+from .forms import ResumeForm, ExperienceForm, EducationForm, JobApplicantForm, ExperienceFormSet, EducationFormSet
 
 
 # Create your views here.
@@ -14,3 +18,40 @@ class MyApplicantProfilesListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return JobApplicant.objects.filter(user_owner=self.request.user)
+    
+
+# # create a new job applicant profile view
+# class JobApplicantCreateView(LoginRequiredMixin, CreateView):
+#     model = JobApplicant
+#     template_name = 'my_resumes_create.html'
+#     fields = ['title', 'resume_file', 'resume', 'education', 'experience']
+#     success_url = reverse_lazy('my_resumes_list')
+
+#     def form_valid(self, form):
+#         form.instance.user_owner = self.request.user
+#         return super().form_valid(form)
+
+
+class JobApplicantCreateView(CreateView):
+    model = JobApplicant
+    form_class = JobApplicantForm
+    template_name = 'my_resumes_create.html'
+    success_url = reverse_lazy('success_page')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['resume_form'] = ResumeForm()
+        context['experience_formset'] = ExperienceFormSet()
+        context['education_formset'] = EducationFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        resume_form = context['resume_form']
+        experience_formset = context['experience_formset']
+        education_formset = context['education_formset']
+
+        if resume_form.is_valid() and experience_formset.is_valid() and education_formset.is_valid():
+            print("done")
+
+        return super().form_valid(form)
