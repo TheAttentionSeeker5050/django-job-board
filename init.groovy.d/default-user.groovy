1b7d717552b41d0fa8cbe0752928ac1926a5321d
nicolas <nicolas.castellano.6@hotmail.com>
2023-11-13 14:53:19 -0400

    fixing jenkins run

--- init.groovy.d/default-user.groovy
+++ init.groovy.d/default-user.groovy
@@ -1,5 +1,7 @@
 import jenkins.model.*
 import hudson.security.*
+import org.jenkinsci.plugins.matrixauth.AuthorizationMatrixNodeProperty
+import org.jenkinsci.plugins.matrixauth.Authority
 
 def env = System.getenv()
 
@@ -8,6 +10,7 @@ def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 
 hudsonRealm.createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 jenkins.setSecurityRealm(hudsonRealm)
-jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+strategy.add(Jenkins.ADMINISTER, env.JENKINS_USER)
+jenkins.setAuthorizationStrategy(strategy)
 
 jenkins.save()
