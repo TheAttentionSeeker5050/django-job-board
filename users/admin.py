5038cf5d89929a761835023b9e6a181143004b6f
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-11-27 00:11:57 +0000

    added ssl

--- users/admin.py
+++ users/admin.py
@@ -4,9 +4,9 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 # import models
-from .models import CompanyAdmin
+# from .models import CompanyAdmin
 # from django.contrib.auth.models import User
 
 # Register your models here.
-admin.site.register(CompanyAdmin)
+# admin.site.register(CompanyAdmin)
 admin.site.register(CustomUser, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -9,20 +9,19 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    
-
-
+    # add additional fields in here
+    email = models.EmailField(unique=True)
     # return self.email
     def __str__(self):
         return self.email
     
 
-class CompanyAdmin(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    company = models.ForeignKey('companies.Company', on_delete=models.CASCADE)
-    is_admin = models.BooleanField(default=True)
+# class CompanyAdmin(models.Model):
+#     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#     company = models.ForeignKey('companies.Company', on_delete=models.CASCADE)
+#     is_admin = models.BooleanField(default=True)
 
-    def __str__(self):
+#     def __str__(self):
         return self.user + "@" + self.company
 
 
